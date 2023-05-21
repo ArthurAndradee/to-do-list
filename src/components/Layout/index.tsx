ae8f3fdf0460d04124aa79986eefcd3c5fc9c574
arthur andrade <arthurandrad.23@gmail.com>
2023-05-21 17:25:00 -0300

    making things easier to handle

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
 
-import { Container, Header } from './styles';
-import CreateTaskButton from '../createTaskButton';
+import { useState } from 'react';
 
-import CreateTaskContainer from '../createTaskContainer';
+import { 
+  Container, 
+  Header, 
+  CreateTaskButton, 
+  Title,
+  CreateTaskContainer,
+  Input,
+  SearchButton
+ } 
+  from './styles';
 
 import Task from '../task';
 
-import Title from '../title';
-
 const Layout: React.FC = () => {
+  const [showContainer, setShowContainer] = React.useState(false)
+
   return (
       <Container>
         <Header>
-            <Title />
-            <CreateTaskContainer />
-            <CreateTaskButton/>
+            <Title>My tasks</Title>
+            <CreateTaskContainer>
+              <Input placeholder="Your task here"/>
+              <SearchButton>Push task</SearchButton>
+            </CreateTaskContainer>
+            <CreateTaskButton>Add new Task +</CreateTaskButton>
         </Header>
             <Task />
       </Container>
--- src/components/title/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  font-weight: 700;
-  font-size: 150%;
-`;
